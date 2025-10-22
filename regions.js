cc87be4cf0bd3d03a7319b51b41a3b1143cfe6de
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-10-22 16:52:43 -0400

    Create regions.js

---
+++ regions.js
@@ -0,0 +1,47 @@
+// regions.js
+const regions = {
+  "Toshkent": [
+    "Chilonzor",
+    "Yunusobod",
+    "Olmazor",
+    "Yakkasaroy",
+    "Bektemir",
+    "Mirzo Ulug‘bek"
+  ],
+  "Andijon": [
+    "Asaka",
+    "Baliqchi",
+    "Paxtaobod",
+    "Xonobod",
+    "Jalaquduq",
+    "Andijon sh."
+  ],
+  "Farg‘ona": [
+    "Qo‘qon",
+    "Marg‘ilon",
+    "Rishton",
+    "Buvayda",
+    "Dang‘ara"
+  ],
+  "Namangan": [
+    "Namangan sh.",
+    "Pop",
+    "Chortoq",
+    "Uchqo‘rg‘on",
+    "Kosonsoy"
+  ],
+  "Samarqand": [
+    "Urgut",
+    "Ishtixon",
+    "Kattaqo‘rg‘on",
+    "Narpay",
+    "Paxtachi"
+  ],
+  "Buxoro": [
+    "Buxoro sh.",
+    "Vobkent",
+    "Jondor",
+    "Qorako‘l",
+    "G‘ijduvon"
+  ]
+};
