fc7c1d48260e318697376e36c34e623531e84713
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-10-24 10:30:40 -0400

    Update regions.js

--- regions.js
+++ regions.js
@@ -6,7 +6,9 @@ const regions = {
     "Olmazor",
     "Yakkasaroy",
     "Bektemir",
-    "Mirzo Ulug‘bek"
+    "Mirzo Ulug‘bek",
+    "Sergeli",
+    "Shayxontohur"
   ],
   "Andijon": [
     "Asaka",
@@ -14,34 +16,78 @@ const regions = {
     "Paxtaobod",
     "Xonobod",
     "Jalaquduq",
-    "Andijon sh."
+    "Andijon sh.",
+    "Qo‘rg‘ontepa",
+    "Bo‘z"
   ],
   "Farg‘ona": [
     "Qo‘qon",
     "Marg‘ilon",
     "Rishton",
     "Buvayda",
-    "Dang‘ara"
+    "Dang‘ara",
+    "Toshloq",
+    "Farg‘ona sh.",
+    "Oltiariq"
   ],
   "Namangan": [
     "Namangan sh.",
     "Pop",
     "Chortoq",
     "Uchqo‘rg‘on",
-    "Kosonsoy"
+    "Kosonsoy",
+    "Yangiqo‘rg‘on",
+    "Norin",
+    "To‘raqo‘rg‘on"
   ],
   "Samarqand": [
     "Urgut",
     "Ishtixon",
     "Kattaqo‘rg‘on",
     "Narpay",
-    "Paxtachi"
+    "Paxtachi",
+    "Bulung‘ur",
+    "Samarqand sh.",
+    "Tayloq"
   ],
   "Buxoro": [
     "Buxoro sh.",
     "Vobkent",
     "Jondor",
     "Qorako‘l",
-    "G‘ijduvon"
+    "G‘ijduvon",
+    "Romitan",
+    "Shofirkon",
+    "Olot"
+  ],
+  "Qashqadaryo": [
+    "Qarshi",
+    "Shahrisabz",
+    "Koson",
+    "Chiroqchi",
+    "Yakkabog‘",
+    "Kasbi",
+    "Kitob",
+    "G‘uzor"
+  ],
+  "Surxondaryo": [
+    "Termiz",
+    "Denov",
+    "Sho‘rchi",
+    "Boysun",
+    "Muzrabot",
+    "Qumqo‘rg‘on",
+    "Jarqo‘rg‘on",
+    "Sariosiyo"
+  ],
+  "Xorazm": [
+    "Urganch",
+    "Xiva",
+    "Gurlan",
+    "Bog‘ot",
+    "Shovot",
+    "Yangibozor",
+    "Qo‘shko‘pir",
+    "Hazorasp"
   ]
 };
