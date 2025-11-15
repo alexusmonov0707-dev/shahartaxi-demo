c86041c6f969f850df1d0aca0606e88e3bfbae3e
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-15 11:51:04 -0500

    Update regions.js

--- regions.js
+++ regions.js
@@ -1,78 +1,12 @@
+// ===============================
+// VILOYATLAR → TUMANLAR
+// Global o‘zgaruvchi sifatida e’lon qilinadi
+// ===============================
+
 window.regionsData = {
-  "Toshkent sh.": [
-    "Bektemir", "Chilonzor", "Mirobod", "Mirzo Ulug‘bek", "Olmazor",
-    "Sergeli", "Shayxontohur", "Uchtepa", "Yakkasaroy", "Yashnobod", "Yunusobod"
-  ],
-
-  "Toshkent viloyati": [
-    "Bekobod", "Bo‘ka", "Bo‘stonliq", "Chinoz", "Qibray", "Ohangaron",
-    "Oqqo‘rg‘on", "Parkent", "Piskent", "Quyi Chirchiq", "Yangiyo‘l",
-    "Yuqori Chirchiq", "Zangiota"
-  ],
-
-  "Andijon": [
-    "Andijon sh.", "Asaka", "Baliqchi", "Buloqboshi", "Bo‘z", "Jalaquduq",
-    "Izboskan", "Qo‘rg‘ontepa", "Marhamat", "Oltinko‘l", "Paxtaobod",
-    "Shahrixon", "Ulug‘nor", "Xo‘jaobod"
-  ],
-
-  "Farg‘ona": [
-    "Farg‘ona sh.", "Oltiariq", "Beshariq", "Bog‘dod", "Buvaida",
-    "Dang‘ara", "Furqat", "Qo‘qon", "Quva", "Rishton", "So‘x",
-    "Toshloq", "Uchko‘prik", "O‘zbekiston"
-  ],
-
-  "Namangan": [
-    "Namangan sh.", "Chortoq", "Chust", "Kosonsoy", "Mingbuloq",
-    "Norin", "Pop", "To‘raqo‘rg‘on", "Uychi", "Uchqo‘rg‘on", "Yangiqo‘rg‘on"
-  ],
-
-  "Samarqand": [
-    "Samarqand sh.", "Bulung‘ur", "Ishtixon", "Jomboy", "Kattaqo‘rg‘on",
-    "Narpay", "Nurobod", "Oqdaryo", "Paxtachi", "Payariq",
-    "Pastdarg‘om", "Qo‘shrabot", "Samarqand t.", "Tayloq", "Urgut"
-  ],
-
-  "Buxoro": [
-    "Buxoro sh.", "Buxoro t.", "G‘ijduvon", "Jondor", "Kogon",
-    "Olot", "Peshku", "Qorako‘l", "Qorovulbozor", "Romitan", "Shofirkon", "Vobkent"
-  ],
-
-  "Jizzax": [
-    "Jizzax sh.", "Arnasoy", "Baxmal", "Do‘stlik", "Forish",
-    "G‘allaorol", "Mirzacho‘l", "Paxtakor", "Yangiobod", "Zafarobod", "Zarbdor", "Zomin"
-  ],
-
-  "Qashqadaryo": [
-    "Qarshi sh.", "Chiroqchi", "Dehqonobod", "G‘uzor", "Kasbi",
-    "Koson", "Ko‘kdala", "Kitob", "Mirishkor", "Muborak", "Nishon",
-    "Qamashi", "Shahrisabz", "Yakkabog‘"
-  ],
-
-  "Surxondaryo": [
-    "Termiz sh.", "Angor", "Bandixon", "Boysun", "Denov", "Jarqo‘rg‘on",
-    "Muzrabot", "Oltinsoy", "Qiziriq", "Qumqo‘rg‘on", "Sariosiyo",
-    "Sherobod", "Sho‘rchi", "Termiz t."
-  ],
-
-  "Sirdaryo": [
-    "Guliston sh.", "Baxt", "Boyovut", "Hovos", "Mirzaobod",
-    "Oqoltin", "Sayxunobod", "Sardoba", "Sirdaryo", "Shirin", "Yangier"
-  ],
-
-  "Navoiy": [
-    "Navoiy sh.", "Karmana", "Konimex", "Qiziltepa", "Navbahor",
-    "Nurota", "Tomdi", "Uchquduq", "Xatirchi"
-  ],
-
-  "Xorazm": [
-    "Urganch sh.", "Bog‘ot", "Gurlan", "Xiva", "Xonqa", "Hazorasp",
-    "Qo‘shko‘pir", "Shovot", "Yangiariq", "Yangibozor"
-  ],
-
-  "Qoraqalpog‘iston": [
-    "Nukus sh.", "Amudaryo", "Beruniy", "Chimboy", "Ellikqal’a",
-    "Kegeyli", "Mo‘ynoq", "Qanliko‘l", "Qo‘ng‘irot", "Qorao‘zak",
-    "Shumanay", "Taxtako‘pir", "Tortko‘l", "Xo‘jayli"
-  ]
+  "Toshkent": ["Bektemir", "Chilonzor", "Yunusobod", "Sergeli"],
+  "Samarqand": ["Urgut", "Ishtixon", "Narpay", "Payariq"],
+  "Farg‘ona": ["Qo‘qon", "Marg‘ilon", "Beshariq", "Oltiariq"],
+  "Buxoro": ["Vobkent", "G‘ijduvon", "Qorako‘l", "Romitan"],
+  "Namangan": ["Kosonsoy", "Chortoq", "Uchqo‘rg‘on", "Namangan sh."]
 };
