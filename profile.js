3d03ca6a0e9c8387d4583816a4c62d61574a5d2b
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-15 11:24:17 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,6 +1,7 @@
 // ===============================
 // FIREBASE INIT
 // ===============================
+import { regionsData } from "./regions.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
   getAuth,
