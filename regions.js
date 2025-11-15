b2dcbc8d62636927edc826842e2da1784ae9a3b4
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-15 11:23:21 -0500

    Update regions.js

--- regions.js
+++ regions.js
@@ -1,93 +1,78 @@
-// regions.js
-const regions = {
-  "Toshkent": [
-    "Chilonzor",
-    "Yunusobod",
-    "Olmazor",
-    "Yakkasaroy",
-    "Bektemir",
-    "Mirzo Ulug‘bek",
-    "Sergeli",
-    "Shayxontohur"
+export const regionsData = {
+  "Toshkent sh.": [
+    "Bektemir", "Chilonzor", "Mirobod", "Mirzo Ulug‘bek", "Olmazor",
+    "Sergeli", "Shayxontohur", "Uchtepa", "Yakkasaroy", "Yashnobod", "Yunusobod"
   ],
+
+  "Toshkent viloyati": [
+    "Bekobod", "Bo‘ka", "Bo‘stonliq", "Chinoz", "Qibray", "Ohangaron",
+    "Oqqo‘rg‘on", "Parkent", "Piskent", "Quyi Chirchiq", "Yangiyo‘l",
+    "Yuqori Chirchiq", "Zangiota"
+  ],
+
   "Andijon": [
-    "Asaka",
-    "Baliqchi",
-    "Paxtaobod",
-    "Xonobod",
-    "Jalaquduq",
-    "Andijon sh.",
-    "Qo‘rg‘ontepa",
-    "Bo‘z"
+    "Andijon sh.", "Asaka", "Baliqchi", "Buloqboshi", "Bo‘z", "Jalaquduq",
+    "Izboskan", "Qo‘rg‘ontepa", "Marhamat", "Oltinko‘l", "Paxtaobod",
+    "Shahrixon", "Ulug‘nor", "Xo‘jaobod"
   ],
+
   "Farg‘ona": [
-    "Qo‘qon",
-    "Marg‘ilon",
-    "Rishton",
-    "Buvayda",
-    "Dang‘ara",
-    "Toshloq",
-    "Farg‘ona sh.",
-    "Oltiariq"
+    "Farg‘ona sh.", "Oltiariq", "Beshariq", "Bog‘dod", "Buvaida",
+    "Dang‘ara", "Furqat", "Qo‘qon", "Quva", "Rishton", "So‘x",
+    "Toshloq", "Uchko‘prik", "O‘zbekiston"
   ],
+
   "Namangan": [
-    "Namangan sh.",
-    "Pop",
-    "Chortoq",
-    "Uchqo‘rg‘on",
-    "Kosonsoy",
-    "Yangiqo‘rg‘on",
-    "Norin",
-    "To‘raqo‘rg‘on"
+    "Namangan sh.", "Chortoq", "Chust", "Kosonsoy", "Mingbuloq",
+    "Norin", "Pop", "To‘raqo‘rg‘on", "Uychi", "Uchqo‘rg‘on", "Yangiqo‘rg‘on"
   ],
+
   "Samarqand": [
-    "Urgut",
-    "Ishtixon",
-    "Kattaqo‘rg‘on",
-    "Narpay",
-    "Paxtachi",
-    "Bulung‘ur",
-    "Samarqand sh.",
-    "Tayloq"
+    "Samarqand sh.", "Bulung‘ur", "Ishtixon", "Jomboy", "Kattaqo‘rg‘on",
+    "Narpay", "Nurobod", "Oqdaryo", "Paxtachi", "Payariq",
+    "Pastdarg‘om", "Qo‘shrabot", "Samarqand t.", "Tayloq", "Urgut"
   ],
+
   "Buxoro": [
-    "Buxoro sh.",
-    "Vobkent",
-    "Jondor",
-    "Qorako‘l",
-    "G‘ijduvon",
-    "Romitan",
-    "Shofirkon",
-    "Olot"
+    "Buxoro sh.", "Buxoro t.", "G‘ijduvon", "Jondor", "Kogon",
+    "Olot", "Peshku", "Qorako‘l", "Qorovulbozor", "Romitan", "Shofirkon", "Vobkent"
   ],
+
+  "Jizzax": [
+    "Jizzax sh.", "Arnasoy", "Baxmal", "Do‘stlik", "Forish",
+    "G‘allaorol", "Mirzacho‘l", "Paxtakor", "Yangiobod", "Zafarobod", "Zarbdor", "Zomin"
+  ],
+
   "Qashqadaryo": [
-    "Qarshi",
-    "Shahrisabz",
-    "Koson",
-    "Chiroqchi",
-    "Yakkabog‘",
-    "Kasbi",
-    "Kitob",
-    "G‘uzor"
+    "Qarshi sh.", "Chiroqchi", "Dehqonobod", "G‘uzor", "Kasbi",
+    "Koson", "Ko‘kdala", "Kitob", "Mirishkor", "Muborak", "Nishon",
+    "Qamashi", "Shahrisabz", "Yakkabog‘"
   ],
+
   "Surxondaryo": [
-    "Termiz",
-    "Denov",
-    "Sho‘rchi",
-    "Boysun",
-    "Muzrabot",
-    "Qumqo‘rg‘on",
-    "Jarqo‘rg‘on",
-    "Sariosiyo"
+    "Termiz sh.", "Angor", "Bandixon", "Boysun", "Denov", "Jarqo‘rg‘on",
+    "Muzrabot", "Oltinsoy", "Qiziriq", "Qumqo‘rg‘on", "Sariosiyo",
+    "Sherobod", "Sho‘rchi", "Termiz t."
   ],
+
+  "Sirdaryo": [
+    "Guliston sh.", "Baxt", "Boyovut", "Hovos", "Mirzaobod",
+    "Oqoltin", "Sayxunobod", "Sardoba", "Sirdaryo", "Shirin", "Yangier"
+  ],
+
+  "Navoiy": [
+    "Navoiy sh.", "Karmana", "Konimex", "Qiziltepa", "Navbahor",
+    "Nurota", "Tomdi", "Uchquduq", "Xatirchi"
+  ],
+
   "Xorazm": [
-    "Urganch",
-    "Xiva",
-    "Gurlan",
-    "Bog‘ot",
-    "Shovot",
-    "Yangibozor",
-    "Qo‘shko‘pir",
-    "Hazorasp"
+    "Urganch sh.", "Bog‘ot", "Gurlan", "Xiva", "Xonqa", "Hazorasp",
+    "Qo‘shko‘pir", "Shovot", "Yangiariq", "Yangibozor"
+  ],
+
+  "Qoraqalpog‘iston": [
+    "Nukus sh.", "Amudaryo", "Beruniy", "Chimboy", "Ellikqal’a",
+    "Kegeyli", "Mo‘ynoq", "Qanliko‘l", "Qo‘ng‘irot", "Qorao‘zak",
+    "Shumanay", "Taxtako‘pir", "Tortko‘l", "Xo‘jayli"
   ]
 };
