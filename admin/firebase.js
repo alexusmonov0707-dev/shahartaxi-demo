f78575e409114bdeb8516edffa2f458c2fa0d355
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-19 18:50:55 -0500

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,36 +1,16 @@
-// ---- Firebase Core ----
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
-// ---- Auth (kerak emas, lekin qolaversin) ----
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-// ---- Database ----
-import {
-  getDatabase,
-  ref,
-  get,
-  set,
-  update,
-  remove,
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-
-// ---- YOUR CONFIG ----
 const firebaseConfig = {
-  apiKey: "AIzaSyAplUAG4vqCsE9MOLXwLCyRiEhREwvc",
-  authDomain: "shahartaxi-demo.firebaseapp.com",
-  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com/",
-  projectId: "shahartaxi-demo",
-  storageBucket: "shahartaxi-demo.appspot.com",
-  messagingSenderId: "874241795701",
-  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
+    apiKey: "AIzaSyApiU4G4VQyCa5EM0LXwLCy8iRhREWvc",
+    authDomain: "shahartaxi-demo.firebaseapp.com",
+    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
+    projectId: "shahartaxi-demo",
+    storageBucket: "shahartaxi-demo.appspot.com",
+    messagingSenderId: "874241795701",
+    appId: "1:874241795701:web:89e9b20a3aed2ad8cebac3"
 };
 
-// ---- INIT ----
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-export { ref, get, set, update, remove };
+export { ref, get };
