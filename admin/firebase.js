6f94456ac3062a5a8eddfc486dffc22066a587bd
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-19 17:59:05 -0500

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,33 +1,30 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+// === FIREBASE MODULE IMPORTS ===
+import { initializeApp } 
+    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-import {
-  getDatabase,
-  ref,
-  get
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged }
+    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import { getDatabase, ref, get }
+    from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 
-// === YOUR CONFIG (o'zingdagi kabi qoldiraman)
+// === YOUR FIREBASE CONFIG ===
 const firebaseConfig = {
-  apiKey: "AIzaSyApWUG40YuC9aCsE9MOLXwLcYgRihREWvc",
-  authDomain: "shahartaxi-demo.firebaseapp.com",
-  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
-  projectId: "shahartaxi-demo",
-  storageBucket: "shahartaxi-demo.firebasestorage.app",
-  messagingSenderId: "874241795701",
-  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
+    apiKey: "AIzaSyApU4G4VVQaCsEM0LXwLCy8RihREwvc",
+    authDomain: "shahartaxi-demo.firebaseapp.com",
+    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
+    projectId: "shahartaxi-demo",
+    storageBucket: "shahartaxi-demo.firebasestorage.app",
+    messagingSenderId: "874241795701",
+    appId: "1:874241795701:web:89e9b20a3aed2ad8ceb3ac"
 };
 
 
-// === INIT
+// === INIT FIREBASE ===
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
-export { signInWithEmailAndPassword, signOut, onAuthStateChanged, ref, get };
 
+// EXPORTS for other files (admin.js)
+export { ref, get, signInWithEmailAndPassword, signOut, onAuthStateChanged };
