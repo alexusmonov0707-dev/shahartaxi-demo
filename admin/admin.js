f5e5d5fc0fcee068afbdfa51cb447a8972aa0bdb
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-19 18:12:14 -0500

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,33 +1,34 @@
 import { db, ref, get } from "./firebase.js";
 
 window.loginAdmin = async function () {
-    const login = document.getElementById("login").value.trim();
-    const pass = document.getElementById("pass").value.trim();
-    const error = document.getElementById("error");
+  const login = document.getElementById("login").value.trim();
+  const pass = document.getElementById("pass").value.trim();
+  const error = document.getElementById("error");
 
-    error.textContent = "";
+  error.textContent = "";
 
-    try {
-        const adminRef = ref(db, "admins/" + login);
-        const snapshot = await get(adminRef);
+  try {
+    // firebase database → /admins/admin001
+    const adminRef = ref(db, "admins/" + login);
+    const snapshot = await get(adminRef);
 
-        if (!snapshot.exists()) {
-            error.textContent = "Login yoki parol noto‘g‘ri!";
-            return;
-        }
-
-        const admin = snapshot.val();
-
-        if (admin.password !== pass) {
-            error.textContent = "Login yoki parol noto‘g‘ri!";
-            return;
-        }
-
-        localStorage.setItem("admin", login);
-        window.location.href = "./dashboard.html";
-
-    } catch (err) {
-        console.error(err);
-        error.textContent = "Server xatosi!";
+    if (!snapshot.exists()) {
+      error.textContent = "Login yoki parol noto‘g‘ri!";
+      return;
     }
+
+    const admin = snapshot.val();
+
+    if (admin.password !== pass) {
+      error.textContent = "Login yoki parol noto‘g‘ri!";
+      return;
+    }
+
+    localStorage.setItem("admin", login);
+    window.location.href = "./dashboard.html";
+
+  } catch (err) {
+    console.error(err);
+    error.textContent = "Server xatosi!";
+  }
 };
