237a44eb81319e7fe395902bc969e453409228da
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-19 18:51:19 -0500

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,34 +1,36 @@
 import { db, ref, get } from "./firebase.js";
 
 window.loginAdmin = async function () {
-  const login = document.getElementById("login").value.trim();
-  const pass = document.getElementById("pass").value.trim();
-  const error = document.getElementById("error");
+    const login = document.getElementById("login").value.trim();
+    const pass = document.getElementById("pass").value.trim();
+    const error = document.getElementById("error");
 
-  error.textContent = "";
+    error.textContent = "";
 
-  try {
-    // firebase database → /admins/admin001
-    const adminRef = ref(db, "admins/" + login);
-    const snapshot = await get(adminRef);
+    try {
+        // admnlar pathni TEKSHIRAMIZ
+        // /admins/admin001
+        const adminRef = ref(db, "admins/" + login);
+        const snap = await get(adminRef);
 
-    if (!snapshot.exists()) {
-      error.textContent = "Login yoki parol noto‘g‘ri!";
-      return;
+        if (!snap.exists()) {
+            error.textContent = "Login yoki parol noto‘g‘ri!";
+            return;
+        }
+
+        const admin = snap.val();
+
+        if (!admin.password || admin.password !== pass) {
+            error.textContent = "Login yoki parol noto‘g‘ri!";
+            return;
+        }
+
+        // login muvaffaqiyatli
+        localStorage.setItem("admin", login);
+        window.location.href = "./dashboard.html";
+
+    } catch (err) {
+        console.error(err);
+        error.textContent = "Server xatosi!";
     }
-
-    const admin = snapshot.val();
-
-    if (admin.password !== pass) {
-      error.textContent = "Login yoki parol noto‘g‘ri!";
-      return;
-    }
-
-    localStorage.setItem("admin", login);
-    window.location.href = "./dashboard.html";
-
-  } catch (err) {
-    console.error(err);
-    error.textContent = "Server xatosi!";
-  }
 };
