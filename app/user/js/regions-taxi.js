65fde6ddb77d895afb17d2008b88fe4487f5c6ba
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-20 12:58:44 -0500

    Create regions-taxi.js

---
+++ app/user/js/regions-taxi.js
@@ -0,0 +1,16 @@
+regions-taxi.js// Taxi bo‘limi uchun viloyatlar va tumanlar
+window.taxiRegionsData = {
+  "Toshkent shahri": ["Chilonzor", "Bektemir", "Mirzo Ulug‘bek", "Shayxontohur", "Olmazor", "Uchtepa", "Yunusobod", "Yakkasaroy"],
+  "Toshkent viloyati": ["Oqqo'rg‘on", "Parkent", "Piskent", "Chinoz", "Nurafshon", "Bekobod", "O‘rtachirchiq"],
+  "Andijon": ["Asaka", "Shaxrixon", "Jalaquduq", "Qo‘rg‘ontepa"],
+  "Farg‘ona": ["Qo‘qon", "Marg‘ilon", "Beshariq", "Oltiariq"],
+  "Namangan": ["Chortoq", "Uychi", "Kosonsoy", "Namangan shahri"],
+  "Samarqand": ["Payariq", "Jomboy", "Kattaqo‘rg‘on", "Urgut"],
+  "Buxoro": ["G‘ijduvon", "Vobkent", "Shofirkon"],
+  "Qashqadaryo": ["Qarshi", "Shahrisabz", "Koson"],
+  "Surxondaryo": ["Denov", "Sherobod", "Boysun"],
+  "Xorazm": ["Urganch", "Xiva", "Gurlan"],
+  "Navoiy": ["Zarafshon", "Konimex", "Nurota"],
+  "Jizzax": ["Zomin", "Gallaorol", "Forish"],
+  "Sirdaryo": ["Guliston", "Sirdaryo", "Yangiyer"]
+};
