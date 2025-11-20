91926a74cd4cff205d25b94ecec377869482d89f
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-20 06:32:44 -0500

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -9,26 +9,25 @@ window.loginAdmin = async function () {
 
     try {
         const adminRef = ref(db, "admins/" + login);
-        const snapshot = await get(adminRef);
+        const snap = await get(adminRef);
 
-        if (!snapshot.exists()) {
+        if (!snap.exists()) {
             error.textContent = "Login yoki parol noto‘g‘ri!";
             return;
         }
 
-        const admin = snapshot.val();
+        const admin = snap.val();
 
         if (admin.password !== pass) {
             error.textContent = "Login yoki parol noto‘g‘ri!";
             return;
         }
 
-        // Login OK
         localStorage.setItem("admin", login);
         window.location.href = "./dashboard.html";
 
     } catch (e) {
-        console.error(e);
+        console.log(e);
         error.textContent = "Server xatosi!";
     }
 };
