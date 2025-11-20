10ce6424118769609a3ac6b2136c410d8ea2172a
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-19 19:02:22 -0500

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,16 +1,33 @@
+// Firebase App
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
+// Realtime Database
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  update,
+  remove,
+  query,
+  orderByChild,
+  equalTo
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+// CONFIG
 const firebaseConfig = {
-    apiKey: "AIzaSyApiU4G4VQyCa5EM0LXwLCy8iRhREWvc",
-    authDomain: "shahartaxi-demo.firebaseapp.com",
-    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
-    projectId: "shahartaxi-demo",
-    storageBucket: "shahartaxi-demo.appspot.com",
-    messagingSenderId: "874241795701",
-    appId: "1:874241795701:web:89e9b20a3aed2ad8cebac3"
+  apiKey: "AIzaSyAplUAG4vqCsE9MOLXwLCyRiEhREwvc",
+  authDomain: "shahartaxi-demo.firebaseapp.com",
+  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com/",
+  projectId: "shahartaxi-demo",
+  storageBucket: "shahartaxi-demo.appspot.com",
+  messagingSenderId: "874241795701",
+  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
 };
 
-const app = initializeApp(firebaseConfig);
+// INIT
+export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-export { ref, get };
+
+// EXPORTS
+export { ref, get, set, update, remove, query, orderByChild, equalTo };
