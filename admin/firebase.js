7245e044d04ec28ddcd4f94909f380facfececaa
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-20 06:32:26 -0500

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,15 +1,14 @@
-// Firebase config (o'zingdagi qiymatlarni qo'y)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "shahartaxi-demo.firebaseapp.com",
-    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
-    projectId: "shahartaxi-demo",
-    storageBucket: "shahartaxi-demo.appspot.com",
-    messagingSenderId: "xxxxxxx",
-    appId: "xxxxxxx"
+  apiKey: "API_KEY",
+  authDomain: "shahartaxi-demo.firebaseapp.com",
+  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
+  projectId: "shahartaxi-demo",
+  storageBucket: "shahartaxi-demo.appspot.com",
+  messagingSenderId: "00000000",
+  appId: "1:00000:web:00000"
 };
 
 const app = initializeApp(firebaseConfig);
