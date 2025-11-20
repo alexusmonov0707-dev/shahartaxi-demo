c72d50101163d89711c88c5797173814b5a6fd1e
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-19 19:25:34 -0500

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,28 +1,28 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
 import {
   getDatabase,
   ref,
-  get,
-  set,
-  update,
-  remove,
-  query,
-  orderByChild,
-  equalTo
+  get
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAplUAG4vqCsE9MOLXwLCyRiEhREwvc",
+  apiKey: "AIzaSyApUi4GQVqCsE9MOLXwLCy8RihREWvc",
   authDomain: "shahartaxi-demo.firebaseapp.com",
   databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com/",
   projectId: "shahartaxi-demo",
   storageBucket: "shahartaxi-demo.appspot.com",
   messagingSenderId: "874241795701",
-  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
+  appId: "1:874241795701:web:89e9b20a3aed2ad8cebac"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getDatabase(app);
-
-export { ref, get, set, update, remove, query, orderByChild, equalTo };
+export { ref, get };
