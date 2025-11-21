6515ae64a834dc784427310e1ad3e1b6d62d1eed
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-21 17:31:25 -0500

    Update lib.js

--- app/user/js/lib.js
+++ app/user/js/lib.js
@@ -1,116 +1,66 @@
-// =============================
-//   FIREBASE INIT
-// =============================
+// ===============================
+// FIREBASE IMPORTS
+// ===============================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import {
-    initializeApp
-} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 import {
-    getAuth,
-    RecaptchaVerifier,
-    signInWithPhoneNumber,
-    signOut as firebaseSignOut
-} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  push,
+  remove,
+  onValue
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-import {
-    getDatabase,
-    ref,
-    set,
-    get,
-    update,
-    onValue
-} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
-
-// =============================
-//   FIREBASE CONFIG
-// =============================
+// ===============================
+// FIREBASE CONFIG
+// ===============================
 const firebaseConfig = {
-    apiKey: "AIzaSyApWUG4YuC9aCsE9MOLXwLcYgRihREWvc",
-    authDomain: "shahartaxi-demo.firebaseapp.com",
-    projectId: "shahartaxi-demo",
-    storageBucket: "shahartaxi-demo.appspot.com",
-    messagingSenderId: "874241795701",
-    appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c",
-    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyApWUG4QYuC9aC5E9MOLXwlCv9RihREWvc",
+  authDomain: "shahartaxi-demo.firebaseapp.com",
+  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
+  projectId: "shahartaxi-demo",
+  storageBucket: "shahartaxi-demo.appspot.com",
+  messagingSenderId: "874241795701",
+  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
 };
 
-// Init
+// ===============================
+// INIT
+// ===============================
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-// =============================
-//   AUTH HELPERS
-// =============================
+// ===============================
+// HELPER $ FUNCTION
+// ===============================
+export const $ = id => document.getElementById(id);
 
-// ReCaptcha yaratish
-export function createRecaptcha(containerId) {
-    return new RecaptchaVerifier(auth, containerId, {
-        size: "invisible"
-    });
-}
-
-// SMS yuborish
-export async function sendCode(phone, recaptcha) {
-    return await signInWithPhoneNumber(auth, phone, recaptcha);
-}
-
-// Kodni tasdiqlash
-export async function verifyCode(confirmationResult, code) {
-    return await confirmationResult.confirm(code);
-}
-
-// Tizimdan chiqish
-export async function signOut() {
-    return await firebaseSignOut(auth);
-}
-
-// =============================
-// DATABASE HELPERS
-// =============================
-
-// User yaratish
-export function createUser(uid, data) {
-    return set(ref(db, "users/" + uid), data);
-}
-
-// Userni olish
-export function getUser(uid) {
-    return get(ref(db, "users/" + uid));
-}
-
-// Userni yangilash
-export function updateUser(uid, data) {
-    return update(ref(db, "users/" + uid), data);
-}
-
-// Real-time listener
-export function listenUser(uid, callback) {
-    return onValue(ref(db, "users/" + uid), (snap) => {
-        callback(snap.val());
-    });
-}
-
-// Ads yaratish / olsh / yangilash
-export function createAd(id, data) {
-    return set(ref(db, "ads/" + id), data);
-}
-
-export function getAds() {
-    return get(ref(db, "ads"));
-}
-
-export function listenAds(callback) {
-    return onValue(ref(db, "ads"), snap => callback(snap.val()));
-}
-
-// Export Firebase obyektlari
+// ===============================
+// EXPORTS (firebase + utils)
+// ===============================
 export {
-    auth,
-    db,
-    ref,
-    get,
-    set,
-    update,
-    onValue
+  auth,
+  db,
+  ref,
+  set,
+  get,
+  update,
+  push,
+  remove,
+  onValue,
+  signOut,
+  onAuthStateChanged,
+  RecaptchaVerifier,
+  signInWithPhoneNumber
 };
