d1ef60c4f77877bb448db7710376086e68eaf5f1
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-22 16:09:54 -0500

    Update lib.js

--- libs/lib.js
+++ libs/lib.js
@@ -1,34 +1,33 @@
-// ===========================
-//  FIREBASE CONFIG
-// ===========================
-
+// Firebase APP
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
+// Auth
 import {
-  getAuth,
-  RecaptchaVerifier,
-  signInWithPhoneNumber,
-  onAuthStateChanged,
-  setPersistence,
-  browserLocalPersistence
+    getAuth,
+    RecaptchaVerifier,
+    signInWithPhoneNumber,
+    onAuthStateChanged,
+    setPersistence,
+    browserLocalPersistence
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+// Database
 import {
-  getDatabase,
-  ref,
-  get,
-  set,
-  update,
-  push
+    getDatabase,
+    ref,
+    get,
+    set,
+    update,
+    push
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApWUG40YuC9aCsE9MOLXwLcYgRihREWvc",
-  authDomain: "shahartaxi-demo.firebaseapp.com",
-  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
-  projectId: "shahartaxi-demo",
-  messagingSenderId: "874241795701",
-  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
+    apiKey: "AIzaSyApWU64QYuc9CsE9MOLXwLcyRihREWvc",
+    authDomain: "shahartaxi-demo.firebaseapp.com",
+    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
+    projectId: "shahartaxi-demo",
+    messagingSenderId: "874241795701",
+    appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -38,14 +37,14 @@ const db = getDatabase(app);
 await setPersistence(auth, browserLocalPersistence);
 
 export {
-  auth,
-  db,
-  ref,
-  get,
-  set,
-  update,
-  push,
-  onAuthStateChanged,
-  RecaptchaVerifier,
-  signInWithPhoneNumber
+    auth,
+    db,
+    ref,
+    get,
+    set,
+    update,
+    push,
+    onAuthStateChanged,
+    RecaptchaVerifier,
+    signInWithPhoneNumber
 };
