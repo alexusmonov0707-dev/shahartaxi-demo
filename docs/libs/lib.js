11fe368a616e3cbda0eeabe47520ef3f5b114fd5
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-22 19:22:06 -0500

    Update lib.js

--- docs/libs/lib.js
+++ docs/libs/lib.js
@@ -1,23 +1,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
+  getAuth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  onAuthStateChanged,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
   getDatabase,
   ref,
-  push,
   set,
   get,
+  push,
   update
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
+// --- SENING TO‘G‘RI FIREBASE CONFIG’ING ---
 const firebaseConfig = {
-  apiKey: "AIzaSyApUUG40YCgc59M0LzWlcYG9iRheiVCw",
+  apiKey: "AIzaSyApWUG40YuC9aCsE9M0LXwLcYgRihREWvc",
   authDomain: "shahartaxi-demo.firebaseapp.com",
   databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
   projectId: "shahartaxi-demo",
   storageBucket: "shahartaxi-demo.appspot.com",
-  messagingSenderId: "87421470521",
-  appId: "1:87421470521:web:xxxxx"
+  messagingSenderId: "874241795701",
+  appId: "1:874241795701:web:89e9b20a3aed2ad8ceba3c"
 };
 
-const app = initializeApp(firebaseConfig);
+// --- App’ni ishga tushirish ---
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getDatabase(app);
-export { ref, push, set, get, update };
+export {
+    ref, set, get, push, update,
+    RecaptchaVerifier, signInWithPhoneNumber,
+    onAuthStateChanged, setPersistence, browserLocalPersistence
+};
