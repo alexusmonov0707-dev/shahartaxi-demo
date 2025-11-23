4946c3a4a0496bd4d0322e27565659fec53e105b
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-22 19:17:25 -0500

    Update lib.js

--- docs/libs/lib.js
+++ docs/libs/lib.js
@@ -1,27 +1,23 @@
-// Firebase initialization
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import {
-    getDatabase,
-    ref,
-    push,
-    set
+  getDatabase,
+  ref,
+  push,
+  set,
+  get,
+  update
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAhpU...YOUR_KEY",
-    authDomain: "shahartaxi-demo.firebaseapp.com",
-    databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
-    projectId: "shahartaxi-demo",
-    storageBucket: "shahartaxi-demo.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abc123"
+  apiKey: "AIzaSyApUUG40YCgc59M0LzWlcYG9iRheiVCw",
+  authDomain: "shahartaxi-demo.firebaseapp.com",
+  databaseURL: "https://shahartaxi-demo-default-rtdb.firebaseio.com",
+  projectId: "shahartaxi-demo",
+  storageBucket: "shahartaxi-demo.appspot.com",
+  messagingSenderId: "87421470521",
+  appId: "1:87421470521:web:xxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-
-// Export qilinadigan funksiya
-export function pushData(path, data) {
-    const newRef = push(ref(db, path));
-    return set(newRef, data);
-}
+export { ref, push, set, get, update };
