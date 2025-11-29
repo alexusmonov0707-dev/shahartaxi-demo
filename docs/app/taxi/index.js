6181f166cad79dcea48f709b7014a71febe7d870
alexusmonov0707-dev <alexusmonov0707@gmail.com>
2025-11-29 09:25:27 -0500

    Update index.js

--- docs/app/taxi/index.js
+++ docs/app/taxi/index.js
@@ -668,3 +668,12 @@ async function startApp() {
 
 // Export start function
 export { startApp };
+function escapeHtml(str) {
+    if (!str) return "";
+    return String(str)
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
